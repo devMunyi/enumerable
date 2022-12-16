07e561359432fe70f7c57d4cb16fcd1141d19b7f
devMunyi <samunyi90@gmail.com>
2022-12-16 12:08:04 +0300

    fix linter errors

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,4 @@
-require_relative "./MyEnumerable.rb";
+require_relative "./my_enumerable.rb";
 
 class MyList
   include MyEnumerable;
@@ -9,7 +9,7 @@ class MyList
 
   def each(&block)
     @list.each(&block);
-    self; 
+    self;
   end
 end
 
@@ -18,19 +18,17 @@ list = MyList.new(1, 2, 3, 4)
 # <MyList: @list=[1, 2, 3, 4]>
 
 # Test #all?
-p list.all? {|e| e < 5}
+p list.all? { |e| e < 5 }
 # => true
-p list.all? {|e| e > 5}
+p list.all? { |e| e > 5 }
 # => false
 
 # Test #any?
-p list.any? {|e| e == 2}
+p list.any? { |e| e == 2 }
 # => true
-p list.any? {|e| e == 5}
+p list.any? { |e| e == 5 }
 # => false
 
 # Test #filter
-p list.filter {|e| e.even?}
+p list.filter { |e| e.even? }
 # => [2, 4]
-
-
